a2178badb6b86fa9ae01cbd55e9c13e7a3276da6
kashrest <theelegantworld@gmail.com>
2023-07-06 02:55:48 -0700

    Some updates

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python wav2vec2-finetuning-hausa.py "facebook/mms-1b-all" "hausa-fleurs-only-1" 16 3e-4 20 0.1 0.1 0.0 0.05 0.1 500 fleurs
+#python wav2vec2-finetuning-hausa.py "facebook/mms-1b-all" "hausa-fleurs-only-1" 16 3e-4 20 0.1 0.1 0.0 0.05 0.1 500 fleurs
 
 python wav2vec2-finetuning-hausa.py "facebook/wav2vec2-xls-r-1b" "hausa-fleurs-only-hyperparameter-1" 16 2e-4 20 0.1 0.1 0.0 0.05 0.1 500 fleurs
 
 python wav2vec2-finetuning-hausa.py "facebook/mms-1b" "hausa-combined-2" 16 3e-4 20 0.1 0.1 0.0 0.05 0.1 500 combined 
 
-python wav2vec2-finetuning-hausa.py "facebook/mms-1b-all" "hausa-combined-1" 16 3e-4 3 0.1 0.1 0.0 0.05 0.1 500 combined 
+#python wav2vec2-finetuning-hausa.py "facebook/mms-1b-all" "hausa-combined-1" 16 3e-4 3 0.1 0.1 0.0 0.05 0.1 500 combined 
 
 python wav2vec2-finetuning-hausa.py "facebook/wav2vec2-xls-r-1b" "hausa-combined-hyperparameter-1" 16 2e-4 3 0.1 0.1 0.0 0.05 0.1 500 combined
